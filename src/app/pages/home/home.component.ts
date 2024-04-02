f6e02904a33b0e1228d022dce7287ace0b95e9a5
Jorge-Rodrigo <jorgerodrigo.monteiro19@gmail.com>
2024-04-01 21:16:45 -0300

    feat: interpolation, interation

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,12 +1,15 @@
 import { Component } from '@angular/core';
+import { CardsComponent } from '../../components/cards/cards.component';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [],
+  imports: [CardsComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
 export class HomeComponent {
-
+    example = "Bem Vindo"
+    userA = { name: "Alex", email: "alex@email.com", job: "Instrutor de Programação"}
+    userB = { name: "Osvaldo", email: "osvaldo@email.com", job: "Estudante"}
 }
