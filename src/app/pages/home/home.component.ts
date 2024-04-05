3925f31497cdcff90b9c1a75f230217ac2f3b5a8
Jorge-Rodrigo <jorgerodrigo.monteiro19@gmail.com>
2024-04-04 22:21:09 -0300

    feat: ngFor rendering obj

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,15 +1,23 @@
 import { Component } from '@angular/core';
 import { CardsComponent } from '../../components/cards/cards.component';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [CardsComponent],
+  imports: [CardsComponent,CommonModule],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
 export class HomeComponent {
     example = "Bem Vindo"
-    userA = { name: "Alex", email: "alex@email.com", job: "Instrutor de Programação"}
-    userB = { name: "Osvaldo", email: "osvaldo@email.com", job: "Estudante"}
+    userList = [
+      { name: 'Alex', email: 'alex@email.com', job: 'Instrutor de Programação' },
+      { name: 'Osvaldo', email: 'osvaldo@email.com', job: 'Estudante' },
+      {
+        name: 'Maria',
+        email: 'maria@email.com',
+        job: 'Desenvolvedora Front-end',
+      },
+    ];
 }
